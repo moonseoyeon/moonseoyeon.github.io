1134c9f1687b9761bb8fe1c79985c93f1270411a
effortguy <xlcuqhdl@gmail.com>
2019-11-15 14:27:33 +0900

    first commit

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$(function() {
+
+    $(".body-wrapper").fadeIn( 1000 );
+
+    $("#index").click(function() {
+        location.href="index.html";
+    })
+
+    $("#facebook").click(function() {
+        location.href="facebook.html";
+    })
+
+    $("#youtube").click(function() {
+        location.href="youtube.html";
+    })
+
+    $("#instagram").click(function() {
+        location.href="instagram.html";
+    })
+
+    $("#twitter").click(function() {
+        location.href="twitter.html";
+    })
+
+})
